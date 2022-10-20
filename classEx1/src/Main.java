cab509abb623802409e6b295296722f431ff43dc
HyoungMeeKim <mykrishna@naver.com>
2022-10-20 20:23:43 +0900

    class ex1 finish

--- classEx1/src/Main.java
+++ classEx1/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Hero hanzo = new Hero("hanzo", 100, 50, 70);
+        Hero lucio = new Hero("lucio", 80, 100, 40);
+
+        hanzo.move();
+        hanzo.eSkill();
+        System.out.println(hanzo.getDetailInfo());
+
+        lucio.move();
+        lucio.eSkill();
+        System.out.println(lucio.getDetailInfo());
+
+        hanzo.attack(lucio);
+        lucio.attack(hanzo);
+
+        System.out.println(hanzo.getDetailInfo());
+        System.out.println(lucio.getDetailInfo());
     }
 }
+
